29db927a7322b0b6c805c30b284cd3428653159d
Rosseh <rossmterry@gmail.com>
2022-02-06 08:04:41 +0000

    added mousedown event handler

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -50,5 +50,30 @@ namespace WpfApp1
             
             }
         }
+
+        TextBlock lastTextBlockClicked;
+        bool findingMatch = false;
+
+
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock;
+            if (findingMatch == false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                lastTextBlockClicked = textBlock;
+                findingMatch = false;
+            }
+            else if (textBlock.Text == lastTextBlockClicked.Text)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                findingMatch = false;
+            }
+            else 
+            {
+                lastTextBlockClicked.Visibility = Visibility.Visible;
+                findingMatch = false;            
+            }
+        }
     }
 }
